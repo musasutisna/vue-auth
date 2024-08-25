eae04ee382f9e973fc75b52b316ac51a8fe4e8e8
Musa Sutisna <mus4.sutisn4@gmail.com>
2024-08-25 18:29:15 +0700

    Invalid default api logout

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (typeof window.config === 'undefined') {
 window.config.AUTH_API_BASEURL = window.config.AUTH_API_BASEURL || ''
 window.config.AUTH_API_LOGIN = window.config.AUTH_API_LOGIN || '/login'
 window.config.AUTH_API_RECOVERY = window.config.AUTH_API_RECOVERY || '/recovery'
-window.config.AUTH_API_LOGOUT = window.config.AUTH_API_LOGOUT || '/logout'
+window.config.AUTH_API_LOGOUT = window.config.AUTH_API_LOGOUT || '/u/logout'
 window.config.AUTH_API_VERIFY = window.config.AUTH_API_VERIFY || '/u/profile'
 window.config.AUTH_TOKEN = window.config.AUTH_TOKEN || ''
 window.config.AUTH_LANG = window.config.AUTH_LANG || {}
